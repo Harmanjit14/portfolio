221d88f74953962d0e93a3009ac1b17f7cd391c7
Harmanjit Singh <harmanjit140500@gmail.com>
2020-12-25 05:03:37 +0530

    navbar2

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:line_awesome_icons/line_awesome_icons.dart';
 import 'package:portfolio/constants/navbarItem.dart';
 import 'package:portfolio/constants/text_style.dart';
 
@@ -10,6 +11,7 @@ class LandingPage extends StatefulWidget {
 class _LandingPageState extends State<LandingPage> {
   @override
   Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Color(0xff03030F),
       body: Column(
@@ -17,7 +19,7 @@ class _LandingPageState extends State<LandingPage> {
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
           Container(
-            margin: EdgeInsets.fromLTRB(50, 10, 50, 0),
+            margin: EdgeInsets.fromLTRB(30, 10, 30, 0),
             // height: 100,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -26,16 +28,42 @@ class _LandingPageState extends State<LandingPage> {
                   child: myText("Harmanjit Singh", Colors.grey[200], 20),
                 ),
                 Container(
-                    child: Row(
-                  children: [
-                    NavbarItem(0, "Projects", Icons.work),
-                    NavbarItem(1, "Achievements", Icons.emoji_events),
-                    NavbarItem(2, "About Me!", Icons.emoji_emotions),
-                  ],
-                ))
+                  child: Row(
+                    children: [
+                      NavbarItem(0, "Projects", Icons.work),
+                      NavbarItem(1, "Achievements", Icons.emoji_events),
+                      NavbarItem(2, "About Me!", Icons.emoji_emotions),
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
+          Expanded(
+            child: Container(
+              child: Stack(children: [
+                Center(
+                  child: headText("Harmanjit Singh", Colors.white, 40),
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(30, 0, 0, 20),
+                  alignment: Alignment.bottomLeft,
+                  child: Column(mainAxisSize: MainAxisSize.min, children: [
+                    IconButton(
+                      icon: Icon(LineAwesomeIcons.github),
+                      onPressed: () {},
+                      padding: EdgeInsets.all(5),
+                      color: Colors.white,
+                      splashColor: Colors.amber,
+                      // hoverColor: Colors.amber,
+                      splashRadius: 20,
+                      iconSize: 30,
+                    )
+                  ]),
+                )
+              ]),
+            ),
+          )
         ],
       ),
     );
