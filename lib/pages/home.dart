50c04864a6df0cbc1fd645ec6db96629344db8ee
Harmanjit Singh <harmanjit140500@gmail.com>
2020-12-27 16:12:44 +0530

    scroll bar added

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -4,6 +4,7 @@ import 'package:portfolio/constants/navbarItem.dart';
 import 'package:portfolio/constants/socialIcons.dart';
 import 'package:portfolio/constants/text_style.dart';
 import 'package:video_player/video_player.dart';
+import 'package:portfolio/constants/scroll.dart';
 
 class LandingPage extends StatefulWidget {
   @override
@@ -13,6 +14,14 @@ class LandingPage extends StatefulWidget {
 class _LandingPageState extends State<LandingPage> {
   VideoPlayerController _controller;
   VideoPlayerController _controller2;
+  ScrollController _scrollController;
+  double _scrollPosition = 0;
+
+  _scrollListener() {
+    setState(() {
+      _scrollPosition = _scrollController.position.pixels;
+    });
+  }
 
   @override
   void initState() {
@@ -25,6 +34,8 @@ class _LandingPageState extends State<LandingPage> {
         'https://firebasestorage.googleapis.com/v0/b/portfolio-21b56.appspot.com/o/work.mp4?alt=media&token=8963d4ab-3169-41f9-b5b4-dba5d31237a4')
       ..initialize();
     _controller2.setLooping(true);
+    _scrollController = ScrollController();
+    _scrollController.addListener(_scrollListener);
   }
 
   @override
@@ -45,8 +56,8 @@ class _LandingPageState extends State<LandingPage> {
           child: FittedBox(
               fit: BoxFit.cover,
               child: Container(
-                  width: _controller.value.size?.width ?? 0,
-                  height: _controller.value.size?.height ?? 0,
+                  width: _controller.value.size.width,
+                  height: _controller.value.size.height,
                   child: VideoPlayer(_controller))),
         );
         break;
@@ -55,8 +66,8 @@ class _LandingPageState extends State<LandingPage> {
           child: FittedBox(
               fit: BoxFit.cover,
               child: Container(
-                  width: _controller.value.size?.width ?? 0,
-                  height: _controller.value.size?.height ?? 0,
+                  width: _controller.value.size.width,
+                  height: _controller.value.size.height,
                   child: VideoPlayer(_controller2))),
         );
         break;
@@ -112,157 +123,195 @@ class _LandingPageState extends State<LandingPage> {
 
   @override
   Widget build(BuildContext context) {
+    var screensize = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Color(0xff03030F),
-      body: Stack(
-        alignment: Alignment.topCenter,
-        children: [
-          showScreen(),
-          Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.start,
+      body: WebScrollbar(
+        color: Colors.yellow,
+        backgroundColor: Colors.transparent,
+        width: 10,
+        heightFraction: 0.3,
+        controller: _scrollController,
+        child: SingleChildScrollView(
+          controller: _scrollController,
+          physics: BouncingScrollPhysics(),
+          child: Column(
             children: [
               Container(
-                margin: EdgeInsets.fromLTRB(30, 10, 30, 0),
-                // height: 100,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                height: screensize.height,
+                width: screensize.width,
+                child: Stack(
+                  alignment: Alignment.topCenter,
                   children: [
-                    Container(
-                      child: myText("Harmanjit Singh", Colors.grey[200], 20),
+                    showScreen(),
+                    Column(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.fromLTRB(30, 10, 30, 0),
+                          // height: 100,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                child: myText(
+                                    "Harmanjit Singh", Colors.grey[200], 20),
+                              ),
+                              Container(
+                                child: Row(
+                                  children: [
+                                    NavbarItem(0, "Projects", Icons.work),
+                                    NavbarItem(
+                                        1, "Achievements", Icons.emoji_events),
+                                    NavbarItem(
+                                        2, "About Me!", Icons.emoji_emotions),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            child: Stack(children: [
+                              Center(
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  mainAxisSize: MainAxisSize.max,
+                                  children: [
+                                    InkWell(
+                                      onHover: (val) {
+                                        // _controller.play();
+                                        setState(() {
+                                          switch (val) {
+                                            case true:
+                                              setState(() {
+                                                index = 2;
+
+                                                _controller2.play();
+                                              });
+
+                                              break;
+                                            case false:
+                                              {
+                                                setState(() {
+                                                  _controller2.pause();
+                                                  index = 0;
+                                                });
+                                              }
+                                              break;
+                                            default:
+                                          }
+                                        });
+                                      },
+                                      onTap: () {},
+                                      child: headText(
+                                          "Applications.", Colors.white, 50),
+                                    ),
+                                    SizedBox(width: 15),
+                                    InkWell(
+                                      onHover: (val) {
+                                        // _controller.play();
+                                        setState(() {
+                                          switch (val) {
+                                            case true:
+                                              setState(() {
+                                                index = 1;
+
+                                                _controller.play();
+                                              });
+
+                                              break;
+                                            case false:
+                                              {
+                                                setState(() {
+                                                  _controller.pause();
+                                                  index = 0;
+                                                });
+                                              }
+                                              break;
+                                            default:
+                                          }
+                                        });
+                                      },
+                                      onTap: () {},
+                                      child: headText(
+                                          "Backend.", Colors.white, 50),
+                                    ),
+                                    SizedBox(width: 15),
+                                    InkWell(
+                                      onHover: (val) {
+                                        // _controller.play();
+                                        setState(() {
+                                          switch (val) {
+                                            case true:
+                                              setState(() {
+                                                index = 1;
+
+                                                _controller.play();
+                                              });
+
+                                              break;
+                                            case false:
+                                              {
+                                                setState(() {
+                                                  _controller.pause();
+                                                  index = 0;
+                                                });
+                                              }
+                                              break;
+                                            default:
+                                          }
+                                        });
+                                      },
+                                      onTap: () {},
+                                      child: headText(
+                                          "Designing.", Colors.white, 50),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Container(
+                                margin: EdgeInsets.fromLTRB(30, 0, 0, 20),
+                                alignment: Alignment.bottomLeft,
+                                child: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      SocialIcon(LineAwesomeIcons.github, 0),
+                                      SizedBox(height: 15),
+                                      SocialIcon(LineAwesomeIcons.instagram, 1),
+                                      SizedBox(height: 15),
+                                      SocialIcon(LineAwesomeIcons.at, 2),
+                                      SizedBox(height: 15),
+                                      SocialIcon(LineAwesomeIcons.linkedin, 3),
+                                      SizedBox(height: 15),
+                                      SocialIcon(LineAwesomeIcons.archive, 4)
+                                    ]),
+                              )
+                            ]),
+                          ),
+                        )
+                      ],
                     ),
-                    Container(
-                      child: Row(
-                        children: [
-                          NavbarItem(0, "Projects", Icons.work),
-                          NavbarItem(1, "Achievements", Icons.emoji_events),
-                          NavbarItem(2, "About Me!", Icons.emoji_emotions),
-                        ],
-                      ),
-                    ),
+                    botText(),
                   ],
                 ),
               ),
-              Expanded(
-                child: Container(
-                  child: Stack(children: [
-                    Center(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisSize: MainAxisSize.max,
-                        children: [
-                          InkWell(
-                            onHover: (val) {
-                              // _controller.play();
-                              setState(() {
-                                switch (val) {
-                                  case true:
-                                    setState(() {
-                                      index = 2;
-
-                                      _controller2.play();
-                                    });
-
-                                    break;
-                                  case false:
-                                    {
-                                      setState(() {
-                                        _controller2.pause();
-                                        index = 0;
-                                      });
-                                    }
-                                    break;
-                                  default:
-                                }
-                              });
-                            },
-                            onTap: () {},
-                            child: headText("Applications.", Colors.white, 50),
-                          ),
-                          SizedBox(width: 15),
-                          InkWell(
-                            onHover: (val) {
-                              // _controller.play();
-                              setState(() {
-                                switch (val) {
-                                  case true:
-                                    setState(() {
-                                      index = 1;
-
-                                      _controller.play();
-                                    });
-
-                                    break;
-                                  case false:
-                                    {
-                                      setState(() {
-                                        _controller.pause();
-                                        index = 0;
-                                      });
-                                    }
-                                    break;
-                                  default:
-                                }
-                              });
-                            },
-                            onTap: () {},
-                            child: headText("Backend.", Colors.white, 50),
-                          ),
-                          SizedBox(width: 15),
-                          InkWell(
-                            onHover: (val) {
-                              // _controller.play();
-                              setState(() {
-                                switch (val) {
-                                  case true:
-                                    setState(() {
-                                      index = 1;
-
-                                      _controller.play();
-                                    });
-
-                                    break;
-                                  case false:
-                                    {
-                                      setState(() {
-                                        _controller.pause();
-                                        index = 0;
-                                      });
-                                    }
-                                    break;
-                                  default:
-                                }
-                              });
-                            },
-                            onTap: () {},
-                            child: headText("Designing.", Colors.white, 50),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.fromLTRB(30, 0, 0, 20),
-                      alignment: Alignment.bottomLeft,
-                      child: Column(mainAxisSize: MainAxisSize.min, children: [
-                        SocialIcon(LineAwesomeIcons.github, 0),
-                        SizedBox(height: 15),
-                        SocialIcon(LineAwesomeIcons.instagram, 1),
-                        SizedBox(height: 15),
-                        SocialIcon(LineAwesomeIcons.at, 2),
-                        SizedBox(height: 15),
-                        SocialIcon(LineAwesomeIcons.linkedin, 3),
-                        SizedBox(height: 15),
-                        SocialIcon(LineAwesomeIcons.archive, 4)
-                      ]),
-                    )
-                  ]),
-                ),
+              Container(
+                height: screensize.height,
+                width: screensize.width,
+                color: Colors.brown,
+              ),
+              Container(
+                height: 300,
+                width: screensize.width,
+                color: Colors.teal,
               )
             ],
           ),
-          botText(),
-        ],
+        ),
       ),
     );
   }
