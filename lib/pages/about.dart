5e20040d662b36049c774d798ad9bf9aa08ebf66
Harmanjit Singh <harmanjit140500@gmail.com>
2020-12-28 22:31:52 +0530

    url launcher added

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -8,8 +8,16 @@ class AboutSection extends StatefulWidget {
 class _AboutSectionState extends State<AboutSection> {
   @override
   Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
     return Container(
-      
+      height: screenSize.height,
+      width: screenSize.width,
+      child: Column(
+        children: [
+          Expanded(flex: 1, child: Container(child: Row())),
+          Expanded(flex: 1, child: Container(child: Row())),
+        ],
+      ),
     );
   }
 }
