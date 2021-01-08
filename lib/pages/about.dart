78f2f50a6cd10d09f1083733eea27357e8592ece
Harmanjit Singh <harmanjit140500@gmail.com>
2021-01-08 07:11:44 +0530

    skills car basic done

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -1,6 +1,9 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
+import 'package:portfolio/constants/skillCard.dart';
 import 'package:portfolio/constants/text_style.dart';
 
 class AboutSection extends StatefulWidget {
@@ -24,46 +27,107 @@ class _AboutSectionState extends State<AboutSection> {
               fit: BoxFit.cover,
             ),
           ),
-          Row(
-            children: [
-              Container(
-                  margin: EdgeInsets.fromLTRB(50, 0, 20, 0),
-                  child: headText("About Me", Colors.black, 50)),
-              Expanded(
-                  child: SingleChildScrollView(
-                child: Container(
-                  margin: EdgeInsets.fromLTRB(30, 0, 50, 0),
-                  width: 400,
-                  child: Column(
-                    // mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("Hello There!",
-                          style: GoogleFonts.poppins(
-                            color: Colors.black,
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                          )),
-                      myText(
-                          "I am Harmanjit Singh. I am Full Stack-Developer cum UI/UX Designer. I am currently pursueing BTech in COE at Thapar Institute of Engineering and Technology, Patiala.",
-                          Colors.black,
-                          22,
-                          weight: FontWeight.w500),
-                      myText(
-                          "I am currently working and ready to collaborate on projects based on Flutter and Django. I usually design UI's on Figma and Adobe Photoshop CC19. I am currently learning AWS.",
-                          Colors.black,
-                          22,
-                          weight: FontWeight.w500),
-                      myText(
-                          "I currently live in Punjab, India and I am happy to work anywhere. My hobbies are swimming, gaming, traveling, badmintion, coding. I am always up to learn something new and create something better for the society",
-                          Colors.black,
-                          22,
-                          weight: FontWeight.w500),
-                    ],
+          Container(
+            margin: EdgeInsets.fromLTRB(50, 20, 50, 20),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 16,
+                          spreadRadius: 10,
+                          color: Colors.black.withOpacity(0.2),
+                        ),
+                      ],
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: BackdropFilter(
+                        filter: ImageFilter.blur(
+                          sigmaX: 7,
+                          sigmaY: 7,
+                        ),
+                        child: Container(
+                          padding: EdgeInsets.all(20),
+                          decoration: BoxDecoration(
+                            color: Colors.white.withOpacity(0.2),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Container(
+                                  margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                                  child:
+                                      headText("About Me", Colors.black, 50)),
+                              myText(
+                                  "Hi There! , My name is Harmanjit Singh. I am Full Stack-Developer cum UI/UX Designer. I am currently pursueing BTech in COE at Thapar Institute of Engineering and Technology, Patiala. I currently live in Punjab, India, happy to work from anywhere. My hobbies are swimming, gaming, traveling, badmintion, coding. I am always up to learn something new and create something better for the society",
+                                  Colors.black,
+                                  22,
+                                  weight: FontWeight.w400),
+                              myText(
+                                  "I am currently working and ready to collaborate on projects based on Flutter and Django. I usually design UI's on Figma and Adobe Photoshop CC19. I am currently learning AWS.",
+                                  Colors.black,
+                                  22,
+                                  weight: FontWeight.w600),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-              ))
-            ],
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 16,
+                          spreadRadius: 10,
+                          color: Colors.black.withOpacity(0.2),
+                        ),
+                      ],
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: BackdropFilter(
+                        filter: ImageFilter.blur(
+                          sigmaX: 7,
+                          sigmaY: 7,
+                        ),
+                        child: Container(
+                          padding: EdgeInsets.all(20),
+                          decoration: BoxDecoration(
+                            color: Colors.white.withOpacity(0.2),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Container(
+                                  margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                                  child: headText(
+                                      "My Skill Set", Colors.black, 50)),
+                              SkillCar(),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         ],
       ),
