063999c33a2a1372d7602b759c91dd06df45d152
Harmanjit Singh <harmanjit140500@gmail.com>
2021-01-12 09:08:54 +0530

    M5

--- lib/pages/education.dart
+++ lib/pages/education.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:line_awesome_icons/line_awesome_icons.dart';
 import 'package:portfolio/constants/text_style.dart';
 
 class EducationPage extends StatelessWidget {
@@ -32,12 +33,46 @@ class EducationPage extends StatelessWidget {
               borderRadius: BorderRadius.circular(20),
             ),
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisSize: MainAxisSize.min,
               children: [
                 Container(
                     margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
                     child: headText("My Skill Set", headColor, 50)),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Container(
+                      child: Icon(
+                        Icons.school,
+                        color: Colors.white,
+                        size: 40,
+                      ),
+                    ),
+                    SizedBox(width: 20),
+                    Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          child: myText("2008-2019", Colors.grey[350], 20),
+                        ),
+                        Container(
+                          child: myText("Matriculation", Colors.white, 30),
+                        ),
+                        Container(
+                          child: myText("M.G.N Public School UE-2,",
+                              Colors.grey[350], 25),
+                        ),
+                        Container(
+                          child: myText("Jalandhar", Colors.grey[350], 20),
+                        ),
+                      ],
+                    )
+                  ],
+                )
                 // SkillCar(),
               ],
             ),
